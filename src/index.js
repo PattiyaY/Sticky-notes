f501b7132e2e78498b66ebd0c6a899acd277f10d
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-04-17 13:14:10 +0700

    feature: create object post it

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-window.addEventListener("load", () => {
-  const container = document.querySelector(".container-input");
-  const input_form = document.querySelector(".input-notes");
-  const list_element = document.querySelector("#text");
-  let storedInput = JSON.parse(localStorage.getItem("textInput")) || [];
+const title = document.getElementById("title_text");
+const content = document.getElementById("content_text");
+const submitTask = document.getElementById("submit");
+const container = document.getElementById(".container");
+const noteLeft = document.getElementById(".notes-left");
+const noteRight = document.getElementById(".notes-right");
+let storedInput = JSON.parse(localStorage.getItem("textInput")) || [];
+let id = 0;
 
-  const saveTasksToLocalStorage = () => {
-    localStorage.setItem("textInput", JSON.stringify(storedInput));
+function newId() {
+  id += 1;
+  return id;
+}
+
+function createPostIt() {
+  const newTask = {
+    id: newId(),
+    task: title.value,
+    content: content.value,
   };
+
+  storedInput.push(newTask);
+  console.log(storedInput);
+}
+
+submitTask.addEventListener("click", () => {
+  createPostIt();
 });
+
+/* TESTTTTTTT */
+// submitTask.addEventListener("click", () => {
+//   console.log(title.value);
+//   console.log(content.value);
+// });
